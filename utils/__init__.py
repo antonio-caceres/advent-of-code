dc907cd81b823c3699122364fac84828b70328a5
Antonio Caceres <antonio@antonio-caceres.com>
2021-12-04 16:24:08 -0800

    [WIP] Clean up namespacing.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,2 @@
 
-from .read import parse_lines
-from .data import data_file
-
-
-def parse_dayta(day, line_parser=str):
-    """Parses the lines from a data file for a given day."""
-    return parse_lines(data_file(day), line_parser)
+from . import read, data
--- utils/read.py
+++ utils/read.py
@@ -1,10 +1,17 @@
 """For reading Advent of Code's puzzle input files into data structures."""
 
+from utils.data import data_file
 
-def parse_lines(file_name, parser=str):
+
+def lines(file_name, parser=str):
     """Parse the lines from a file with a function.
 
     Strips new lines from the end."""
     with open(file_name) as f:
         # remove new lines using splitlines
         return [parser(line) for line in f.read().splitlines(keepends=False)]
+
+
+def dayta(day, line_parser=str):
+    """Parses the lines from a data file for a given day."""
+    return lines(data_file(day), line_parser)
