410d99de98537a50d190e4780d599bb62bd9ceff
Antonio Caceres <antonio@antonio-caceres.com>
2021-12-21 00:25:32 -0600

    Condense functionality of (2021) Day 11: Dumbo Octopus.

--- solutions/yr2021/day11_dumbooctopus.py
+++ solutions/yr2021/day11_dumbooctopus.py
@@ -11,10 +11,12 @@ def _increment_energy(energy_array, index, flashed_stack):
     if energy_array[index] == 10:
         energy_array[index] = 0
         flashed_stack.extend(arr.adjacent(index, energy_array.shape))
+        return True
+    return False
 
 
 def octopus_flashes(energy_array):
-    """Simulate octopus energy growths and flashes in-place (for a single time step).
+    """Simulate octopus energy growths in-place and return flashes (for a single time step).
 
     Octopus energy growth is incremental from energy 0-9.
     If an octopus has energy 9, the next energy increment causes its energy to return to 0,
@@ -34,54 +36,30 @@ def octopus_flashes(energy_array):
     1. The initial increment, when all octopuses gain 1 energy.
     2. The flashed increments, when only adjacent octopuses that have >0 energy gain energy.
     """
-    # After the initial universal +1 energy increase, every flash is equivalent.
-    # (+1 if the octopus is at >0 energy, +0 if the octopus is at 0 energy).
+    num_flashes = 0
     flashed_stack = []
 
     # Increment all octopuses by 1 and initialize the first flash-triggered increments.
     for index in np.ndindex(energy_array.shape):
-        _increment_energy(energy_array, index, flashed_stack)
+        num_flashes += _increment_energy(energy_array, index, flashed_stack)
 
     # Increment octopuses that have not already flashed from the triggers.
     while len(flashed_stack) > 0:
         if energy_array[index := flashed_stack.pop()] > 0:
-            _increment_energy(energy_array, index, flashed_stack)
+            num_flashes += _increment_energy(energy_array, index, flashed_stack)
 
-
-def total_flashes(energy_array, time_steps=1):
-    """Get the total number of octopus flashes after a number of time steps.
-
-    Modifies ``energy_array`` in-place.
-    """
-    flashes = 0
-
-    for _ in range(time_steps):
-        octopus_flashes(energy_array)
-        # counts the number of energies in the array where the value is == 0
-        flashes += np.count_nonzero(energy_array == 0)
-
-    return flashes
-
-
-def first_complete_flash(energy_array):
-    """Get the first step where ``energy_array`` completely flashes (i.e. is all zeros after the step).
-
-    Modifies ``energy_array`` in-place.
-
-    If ``energy_array`` is initially all zeros, return 0.
-    """
-    step_counter = 0
-
-    while np.any(energy_array != 0):
-        step_counter += 1
-        octopus_flashes(energy_array)
-
-    return step_counter
+    return num_flashes
 
 
 if __name__ == "__main__":
-    octopuses = np.array(read.dayta(day=11, line_parser=read.iter_parser(int)))
-    n_flashes = total_flashes(octopuses.copy(), time_steps=100)
-    print(f"Part One: {n_flashes}")
-    first_step = first_complete_flash(octopuses.copy())
-    print(f"Part Two: {first_step}")
+    init_octopuses = np.array(read.dayta(day=11, line_parser=read.iter_parser(int)))
+
+    octopuses = init_octopuses.copy()
+    total_flashes = sum(octopus_flashes(octopuses) for _ in range(100))
+    print(f"Part One: {total_flashes}")
+
+    octopuses = init_octopuses.copy()
+    step_counter = 1  # at least one flash step happens every time
+    while octopus_flashes(octopuses) != len(octopuses):
+        step_counter += 1
+    print(f"Part Two: {step_counter}")
